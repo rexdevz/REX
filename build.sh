37591fe5a3f085d322e8a2f2c8fc367cdb741a7a
REX <rez.devz@gmail.com>
2023-01-27 03:00:31 +0700

    Ginkgo: build: bananadroid tanduk

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rexdevz/local_manifest --depth 1 -b arcane-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rexdevz/local_manifest --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=$BUILD_USERNAME
 export BUILD_HOSTNAME=$BUILD_HOSTNAME
-lunch aosp_ginkgo-userdebug
+lunch banana_ginkgo-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make bacon -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
+m banana > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
